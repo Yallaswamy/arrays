1be2a93e7ae01ed5c56f077cbf482e16b2ccb210
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-01-21 11:11:03 +0530

    Add files via upload

---
+++ sum of elements in the arrays.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	//int n,sum=0;
+//	scanf("%d",&n);
+	int a[5],i,sum=0;
+	//for(i=0;i<5;i++)
+	{
+	scanf("%d",&a[i]);
+     }
+     for(i=0;i<5;i++)
+     {
+     	sum+=a[i];
+	}
+	printf("%d\n",sum);
+}
