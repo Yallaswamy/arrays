597302c0a1ecba2ad7942843a5c7bf354cf4d7a2
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-01-21 10:58:53 +0530

    Add files via upload

---
+++ printf and scanf for arrays.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	int n;
+	scanf("%d",&n);
+	int a[n],i;
+	for(i=0;i<n;i++)
+	{
+	scanf("%d",&a[i]);
+     }
+     for(i=0;i<n;i++)
+     {
+     	printf("%d",a[i]);
+	}
+
+}
